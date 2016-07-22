56a93737d7f820847a941e5f1234c4300ff0e64f
RKoopman <raoul.koopman@gmail.com>
2016-07-22 16:15:56 -0400

    using forEach loop

---
+++ forEach.js
@@ -0,0 +1,16 @@
+function iterativeLog(array){
+  array.forEach(function(item, index, array) {
+    console.log(`${index}: ${item}`)
+  })
+};
+
+function iterate(something) {
+  const boogie = [0, 1, 2, 3, 4, 5]
+  boogie.forEach(something)
+  return boogie
+}
+
+
+function doToArray(array, coolbean){
+  array.forEach(coolbean)
+};
